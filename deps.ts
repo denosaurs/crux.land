4c5623581313d43f9225f45d2b9549605172e27b
crowlkats <13135287+crowlKats@users.noreply.github.com>
2020-11-21 23:10:41 +0100

    fix(deps): export type

--- deps.ts
+++ deps.ts
@@ -1,7 +1,9 @@
 export { ServerRequest } from "https://deno.land/std@0.78.0/http/server.ts";
 export {
-  FormFile,
   MultipartReader,
+} from "https://deno.land/std@0.78.0/mime/mod.ts";
+export type {
+  FormFile,
 } from "https://deno.land/std@0.78.0/mime/mod.ts";
 export * as status from "https://deno.land/x/status/codes.ts";
 export { S3Bucket } from "https://deno.land/x/s3@0.2.1/mod.ts";
