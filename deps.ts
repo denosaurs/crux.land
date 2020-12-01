2675274f067160837c6241f9d2f05a63e7fe9868
Filippo Rossi <pippo.reds@gmail.com>
2020-12-01 13:01:57 +0100

    feat(api): add formatted response

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 export { ServerRequest } from "https://deno.land/std@0.79.0/http/server.ts";
 export { MultipartReader } from "https://deno.land/std@0.79.0/mime/mod.ts";
 export { S3Bucket } from "https://raw.githubusercontent.com/denosaurs/deno_s3/main/mod.ts";
+export type { GetObjectResponse } from "https://raw.githubusercontent.com/denosaurs/deno_s3/main/mod.ts"
 export * as status from "https://deno.land/x/status/codes.ts";
