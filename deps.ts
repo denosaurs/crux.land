945aaed4ef5450972e80d903e14294f659ab86cc
Filippo Rossi <pippo.reds@gmail.com>
2020-12-01 12:30:27 +0100

    chore: fix vercel dev command

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { ServerRequest } from "https://deno.land/std@0.78.0/http/server.ts";
 export { MultipartReader } from "https://deno.land/std@0.78.0/mime/mod.ts";
-export * as status from "https://deno.land/x/status/codes.ts";
 export { S3Bucket } from "https://deno.land/x/s3@0.2.1/mod.ts";
+export * as status from "https://deno.land/x/status/codes.ts";
