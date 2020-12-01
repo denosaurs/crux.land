0f2de294830cfe36c6520b2e99fe7e3bc9c7f11f
Filippo Rossi <12383260+qu4k@users.noreply.github.com>
2020-12-01 16:24:56 +0100

    feat(api): add formatted response (#1)

--- api/get.ts
+++ api/get.ts
@@ -1,4 +1,4 @@
-import { S3Bucket, ServerRequest, status } from "../deps.ts";
+import { S3Bucket, GetObjectResponse, ServerRequest, status } from "../deps.ts";
 import { validate } from "../util/base58.ts";
 import { EXTENSION_FROM_CONTENT_TYPE, EXTENSIONS } from "../util/constants.ts";
 import {
@@ -8,20 +8,59 @@ import {
   invalidMethod,
 } from "../util/responses.ts";
 
-export default async function (req: ServerRequest) {
+type S3File = GetObjectResponse;
+
+async function raw(
+  req: ServerRequest,
+  file: S3File,
+  id: string,
+  ext: string | undefined,
+): Promise<void> {
+
+  if (!ext) {
+    return req.respond({
+      status: status.TEMPORARY_REDIRECT,
+      headers: new Headers({
+        "Location": `./${id}.${EXTENSION_FROM_CONTENT_TYPE[file.contentType!]}`,
+      }),
+    });
+  } else {
+    return req.respond({
+      status: status.OK,
+      headers: new Headers({
+        "Content-Type": `${file.contentType!}; charset=utf-8`,
+      }),
+      body: file.body,
+    });
+  }
+}
+
+async function formatted(
+  req: ServerRequest,
+  file: S3File,
+  id: string,
+  ext: string | undefined,
+): Promise<void> {
+  return req.respond({
+    status: status.OK,
+    headers: new Headers({
+      "Content-Type": 'text/html; charset=utf-8',
+    }),
+    body: file.body,
+  });
+}
+
+export default async function (req: ServerRequest): Promise<void> {
   if (req.method !== "GET") {
     return invalidMethod(req);
   }
 
-  console.log("headers", req.headers);
+  const accept = req.headers.get("accept");
+  const isHtml = accept && accept.indexOf("html") >= 0;
 
   const url = new URL(req.url, "http://crux.land");
-  console.log("url", url);
   const [id, ...rest] = url.pathname.split("/").pop()!.split(".");
   const ext = rest.pop();
-
-  console.log("id", id);
-  console.log("ext", ext);
 
   if (!id || !validate(id)) {
     return invalidId(req);
@@ -44,20 +83,9 @@ export default async function (req: ServerRequest) {
     return fileNotFound(req);
   }
 
-  if (!ext) {
-    return req.respond({
-      status: status.TEMPORARY_REDIRECT,
-      headers: new Headers({
-        "Location": `./${id}.${EXTENSION_FROM_CONTENT_TYPE[file.contentType!]}`,
-      }),
-    });
+  if (isHtml) {
+    return formatted(req, file, id, ext);
   } else {
-    return req.respond({
-      status: status.OK,
-      headers: new Headers({
-        "Content-Type": `${file.contentType!}; charset=utf-8`,
-      }),
-      body: file.body,
-    });
+    return raw(req, file, id, ext);
   }
 }
