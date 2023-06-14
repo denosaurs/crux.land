301c885014a7358286cf6c3fc67efcbbbd7a4a71
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-06-14 14:21:44 -0400

    feat: sticky header

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,7 +2,7 @@ import Features from "./Features.tsx";
 
 export function Content() {
     return (
-        <div class="mt-4 pb-8 flex justify-center rounded-lg text-black text-1xl lg:text-1xl animate-jump bg-primary">
+        <div class="mt-4 pb-8 flex justify-center rounded-lg text-black text-1xl lg:text-1xl bg-primary">
             <Features/>
         </div>
     );
